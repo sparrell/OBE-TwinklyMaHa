ec5afa1db8aed3743323c0574f90767c55f458d8
sigu <sigu@podiihq.com>
2020-07-18 12:11:01 +0300

    phoenix route

--- lib/twinklyMaha_web/router.ex
+++ lib/twinklyMaha_web/router.ex
@@ -18,6 +18,7 @@ defmodule TwinklyMahaWeb.Router do
     pipe_through :browser
 
     live "/", PageLive, :index
+    live "/phoenix", PageLive, :index
   end
 
   # Other scopes may use custom stacks.
